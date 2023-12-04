51f282390f3e4f40002c65cb217d7fe00adeb574
FuTou <futouyiba@gmail.com>
2023-12-04 23:43:11 +0800

    add english content.

---
+++ src/content/experience.js
@@ -0,0 +1,156 @@
+/* eslint max-len: "off" */
+
+const experience = {
+  title: 'Work Experience',
+  icon: null,
+  description: null,
+  list: [
+    {
+      title: 'Producer',
+      subtitle: 'Beijing Southwest-by-Southwest Technology Co., Ltd.',
+      subtitleDetail: ' - Partner in a friend\'s company',
+      date: '2022.01 to present',
+      tags: [
+        'Team Management',
+        'Game Design',
+        'Engineering Development',
+      ],
+      description: [
+        'Managing a 9-person development team, collaborating with company operations, marketing, and iOS & Android engineers.',
+        'Product "Xiugou Nightclub", a game that combines voice chat rooms and launched on all channels.',
+        'Reason for considering leaving: After a period of operation, the users\' willingness to pay is weak. The combination of the game and voice chat rooms leans towards the metaverse, and the entire industry faces problems in the business model, with deeper reasons such as consensus and hardware. It is unrealistic to expect a solution within a year.',
+      ]
+    },
+    {
+      title: 'Producer',
+      subtitle: 'Nianli Cloud Gaming',
+      subtitleDetail: ' - A cloud gaming startup near home',
+      date: '2021.05 - 2022.1',
+      tags: [
+        'Team Management',
+        'Business Strategy',
+        'Engineering Development',
+        'Game Design',
+      ],
+      description: [
+        'Managing an 11-person team',
+        'Product "Cloud-native game project", confidentiality required',
+        'Reason for leaving: The company stopped developing games',
+      ]
+    },
+    {
+      title: 'Producer',
+      subtitle: 'Beijing Kaitian Chuangshi Technology Co., Ltd.',
+      subtitleDetail: ' - A medium-sized game developer near home',
+      date: '2020.05 - 2021.5',
+      tags: [
+        'Team Management',
+        'Business Strategy',
+        'Product Development',
+        'Game Design',
+      ],
+      description: [
+        'Managing a 32-person team',
+        'Researching business strategies, identifying valuable user groups, and forming and leading a team to develop products that meet user needs',
+        'Product "Le is Dungeon", made numerous core mechanism innovations to meet the needs of the target user group',
+        'Positive player feedback on gameplay and art style during testing',
+        'Reason for leaving: Significant disagreements with the publisher on distribution and promotion strategies, causing various issues due to mismatch',
+      ]
+    },
+    {
+      title: 'Founder',
+      subtitle: 'Beijing Hyperbolic Interactive Technology Co., Ltd.',
+      subtitleDetail: ' - Technology startup company',
+      date: '2018.05 - 2020.6',
+      tags: [
+        'Team Management',
+        'Product Design',
+        'Product Development',
+        'Game Design',
+        'Game Development'
+      ],
+      description: [
+        'Leading a 9-person team, overseeing product design, development, and external affairs',
+        'Developed "Yang" patented technology',
+        'Launched "Fanxing" community co-built online game solution based on unique technology',
+        'Established a strategic partnership with DuYou Network',
+        'Developed the sample TCG game "Renaissance" for the "Fanxing" platform',
+        'Developed the sample Web game "Destined Stone" for the "Fanxing" platform'
+      ]
+    },
+    {
+      title: 'Independent Game Producer',
+      subtitle: 'Indie Game Studio',
+      date: '2016.11-2018.04',
+      tags: [
+        'Team Management',
+        'Game Design',
+        'Game Development',
+      ],
+      description: [
+        'Led a 3-person team, efficiently coordinating design, development, and art around game projects.',
+        'Responsible for the project overall vision, specific design, as well as coding the combat and network synchronization.',
+        'Developed the real-time online formation strategy game "Fountain Commander".',
+      ],
+    },
+    {
+      title: 'Lead Game Designer',
+      subtitle: 'Kabam (Beijing) Technology Co., Ltd.',
+      subtitleDetail: ' - American multinational mobile game company',
+      date: '2014.07-2016.11',
+      tags: [
+        'Team Management',
+        'Game Design',
+        'Game Development'
+      ],
+      description: [
+        'Managing a 6-person team, designing gameplay, economic systems, and monetization models',
+        'As a creator, "Marvel: Contest of Champions" generated over $900 million in global revenue. The game is a high-definition 3D card+fighting game with numerous innovations in core combat and peripheral systems.',
+        'As the lead designer for online updates and maintenance of "Heroes of Camelot", a Japanese-style card game targeting the European and American markets, primarily responsible for guild wars and game metrics. Through guild wars, player retention increased by 3%. Briefly developed "Mad Max: Fury Road", an SLG game, as a game tuning designer.',
+      ],
+    },
+    {
+      title: 'Game Tuning Design Team Leader',
+      subtitle: 'Gameloft Beijing Studio',
+      subtitleDetail: ' - The oldest mobile game developer',
+      date: '2012.07-2014.07',
+      tags: [
+        'Game Metrics',
+        'Game Design',
+      ],
+      description: [
+        'Mainly researched game battle balance and economic system-related mathematical models; responsible for the project while providing assistance for multiple projects\' game metrics.',
+        'The "Order & Chaos" series was the first full 3D MMO game on mobile, with series revenue exceeding $30 million. I participated in the updates and maintenance of the first product and the development of the second product from scratch as a game tuning designer.',
+        'Shared heavy game metrics experience from MMOs, assisted in the establishment of game metrics systems for multiple projects within the company. Responsible for recruitment and assessment of game tuning designers.',
+      ],
+    },
+    {
+      title: 'Game Data Analyst',
+      subtitle: 'LeYuan',
+      tags: [
+        'Game Data Analysis',
+      ],
+      description: [
+        'Helped social games mine data performance on Facebook and QZone to improve game content.',
+        'Supported projects include "Happy Bubble Cat," "I Am Legend," "My Kingdom," and "The Rabbit is Coming."'
+      ],
+      subtitleDetail: ' - Domestic social game developer',
+      date: '2012.04-2012.07',
+    },
+    {
+      title: 'Corporate Credit Analyst',
+      subtitle: 'Agricultural Bank of China',
+      tags: [
+        '',
+      ],
+      description: [
+        'Mainly engaged in corporate loan management, clients include Hopson, Pearl River, Poly, Beijing Land Reserve, Sinopharm Group, and Supply and Marketing Group.',
+        'Mainly engaged in corporate loan management, clients include Hopson Development Holdings Limited, Pearl River Real Estate Limited, Poly Real Estate Limited, Beijing Municipal Government Land Reserve Center, Sinopharm Group, and China National Supply and Marketing Cooperative."'
+      ],
+      subtitleDetail: ' - One of the four major state-owned commercial banks',
+      date: '2008.09-2012.03',
+    },
+  ]
+};
+
+export default experience;
