10d44030d3d7b6a53d2cbb3a30a773ff10405915
FuTou <futouyiba@gmail.com>
2024-03-18 22:35:42 +0800

    grab data from another repo.

--- src/content/profile.js
+++ src/content/profile.js
@@ -2,7 +2,7 @@
 
 const profile = {
   title: 'Self Evaluation',
-  description: 'A game production guy with a commercial mindset, skilled in game tuning, having hands-on experience, and once had a successful commercial project experience. 3 years of experience as a producer. Previously worked at Kabam & Gameloft as a Lead Game Designer/Game Tuning Designer, with projects generating over $900 million in revenue. Rational, data-driven, and a deep thinker, with a talent for turning creativity into reality. Comfortable th coding, with strong engineering control.',
+  description: 'A game production guy with a commercial mindset, experienced in coding and software engineering management, having hands-on experience, and had commercial project successes. 3 years of experience as a producer. Previously worked at Kabam & Gameloft as a Lead Game Designer/Game Tuning Designer, with projects generating over $900 million in revenue. Rational, data-driven, and a deep thinker, with a talent for turning creativity into reality. Comfortable with coding, with strong engineering control.',
   icon: 'fas fa-user',
 };
 
